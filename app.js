bbeeb85ac32bb16c34fa34a67c90994d9b327c9e
Doman21 <127606147+Doman21@users.noreply.github.com>
2023-03-11 14:57:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 })
+
+<a href="Programm.html" onclick="window.open('Programm.html', '_self');">
+Programm
+
+
+</a>
